bb4447752f0924036d22e5281d298896a0bf3c37
mac <fajrudinsauqi@gmail.com>
2025-09-05 19:45:14 +0700

    feat: finished materi creation

--- resources/views/livewire/admin/material/index.blade.php
+++ resources/views/livewire/admin/material/index.blade.php
@@ -6,8 +6,27 @@ use App\Models\Material;
 
 new class extends Component {
     public Activity $activity;
+    public $search;
+
     public function mount(Activity $activity){
         $this->activity = $activity;
+        $this->materials = $activity->materials;
+    }
+
+    public function getMaterials(){
+        $materials = Material::where('activity_id', $this->activity->id);
+        if($this->search){
+            $materials->where('title', 'like', '%'.$this->search.'%');
+        }
+
+        return $materials->get();
+    }
+
+    public function with()
+    {
+        return [
+            'materials' => $this->getMaterials(),
+        ];
     }
 }; ?>
 
@@ -19,27 +38,44 @@ new class extends Component {
         <x-nav.breadcrumb-item title='Materi' />
     </x-nav.breadcrumb>
 
-
     <div class="flex items-center mb-8">
         <div>
             <span class="font-semibold text-xl block">Daftar Materi</span>
             <span class="text-sm block text-gray-400">{{$activity->title}}</span>
         </div>
         <flux:spacer />
-        <div class="flex gap-3">
-            <flux:button variant="outline" href="{{ route('admin.material.create', ['activity' => $activity->id]) }}" iconTrailing='plus'>
-                Tambah Materi
-            </flux:button>
-        </div>
+        @role('admin')
+            <div class="flex gap-3">
+                <flux:button variant="outline" href="{{ route('admin.material.create', ['activity' => $activity->id]) }}"
+                    iconTrailing='plus'>
+                    Tambah Materi
+                </flux:button>
+            </div>
+        @endrole
     </div>
 
     <!-- Search and Filter Bar -->
     <div class="flex flex-col sm:flex-row gap-4 items-start sm:items-center mb-8">
         <!-- Search Input -->
         <div class="flex-1 w-full sm:max-w-md">
-            <flux:input wire:model.live.debounce.300ms="search" placeholder="Cari materi..."
-                icon="magnifying-glass" />
+            <flux:input wire:model.live.debounce.300ms="search" placeholder="Cari materi..." icon="magnifying-glass" />
         </div>
+    </div>
+
+    <div class="flex flex-col sm:flex-row gap-4 items-start sm:items-center mb-8">
+        @for ($i = 0; $i < count($materials); $i++) <a
+            href="{{ route('admin.material.detail', ['activity' => $activity->id, 'material' => $materials[$i]->id]) }}"
+            class="flex-1 w-full sm:max-w-xl p-4 border border-gray-300/30 rounded-md flex gap-3 items-center hover:bg-blue-50/10">
+            <div class="rounded-full bg-gray-300/50 flex justify-center items-center w-8 h-8 flex-none">
+                <span class="font-bold">{{$i + 1}}</span>
+            </div>
+            <div class="flex-1">
+                <div class="text-start font-medium">
+                    {{ $materials[$i]->title }}
+                </div>
+            </div>
+            </a>
+            @endfor
     </div>
 
 </div>
